0fbef35d425c621afd6ab320f492cffd73579244
Facturas <eliezelapolinaris2017@gmail.com>
2026-09-22 14:17:34 -0400

    Refresh PWA cache after operational readiness audit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="taller-empleados-v7";
+const C="taller-empleados-v8";
 const ASSETS=["./","index.html","styles.css","app.js","manifest.webmanifest","icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("taller-empleados-")&&k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
