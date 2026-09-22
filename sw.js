1ad50bbe73246e77346f700f27ba54ab2822108d
Facturas <eliezelapolinaris2017@gmail.com>
2026-09-22 19:29:19 -0400

    Refresh employee PWA after warranty updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="taller-empleados-v9";
+const C="taller-empleados-v10";
 const ASSETS=["./","index.html","styles.css","app.js","manifest.webmanifest","icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("taller-empleados-")&&k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
